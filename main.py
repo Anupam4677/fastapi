cd42c068cbda9717021cf8689b9802510f9c77ac
Anupamprakashsingh <anupamprakash01@gmail.com>
2023-05-05 23:32:01 +0530

    API Stablished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -6,3 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is your post"}
+
+@app.post("/createposts")
+def create_posts(payLoad:dict = Body(...)):
+    print(payLoad)
+    return {"new_post":f"title {payLoad['title']} ,content: {payLoad['content']}"}
