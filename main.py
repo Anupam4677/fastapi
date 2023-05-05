f3cd54756ebb5cb571069cec6225648e203a65df
Anupamprakashsingh <anupamprakash01@gmail.com>
2023-05-06 00:05:04 +0530

    Pydantic-Data Validation

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title:str
+    content:str
+    # true is the default value
+    published:bool = True
+    # optional filed , if user dosnt provide it , it ll defalt to None
+    rating:Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -12,6 +21,10 @@ def get_posts():
     return {"data":"This is your post"}
 
 @app.post("/createposts")
-def create_posts(payLoad:dict = Body(...)):
-    print(payLoad)
-    return {"new_post":f"title {payLoad['title']} ,content: {payLoad['content']}"}
+def create_posts(post:Post):
+    # new post is a pydantic model 
+    # can we converted into python dict - post.dict()
+    # print(post.dict())
+    # print(post.published)
+    # print(post.rating)
+    return {"new_post":f"title: {post.title} ,content: {post.content}"}
